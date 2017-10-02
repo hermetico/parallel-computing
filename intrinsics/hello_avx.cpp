94ab826ab0321f3ec6484370df3359c0ea93bae4
hermetico <yojmarinv@gmail.com>
2017-10-02 12:55:52 +0200

    kind of hello world for AVX/AVX2 intrinsics

---
+++ intrinsics/hello_avx.cpp
@@ -0,0 +1,24 @@
+#include <immintrin.h>
+#include <stdio.h>
+
+int main() {
+
+    /* Initialize the two argument vectors */
+    __m256 evens = _mm256_set_ps(2.0, 4.0, 6.0, 8.0, 10.0, 12.0, 14.0, 16.0);
+    __m256 odds = _mm256_set_ps(1.0, 3.0, 5.0, 7.0, 9.0, 11.0, 13.0, 14.0);
+
+    /* Compute the difference between the two vectors
+     * All of are one but the last
+     * */
+    __m256 result = _mm256_sub_ps(evens, odds);
+
+    /* Display the elements of the result vector
+     * As intel is little endian, it stores the values reversed, so the last result (2.0) it's stored
+     * the first
+     * */
+    float* f = (float*)&result;
+    printf("%f %f %f %f %f %f %f %f\n",
+           f[0], f[1], f[2], f[3], f[4], f[5], f[6], f[7]);
+
+    return 0;
+}
