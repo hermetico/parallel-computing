ddcf31491ee3af13b633ba6521f1f951903057c9
hermetico <yojmarinv@gmail.com>
2017-11-01 23:10:33 +0100

    mpi tests

---
+++ particles/mpitests/mpisendrecv.cpp
@@ -0,0 +1,29 @@
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    // Initialize the MPI environment
+    MPI_Init(NULL, NULL);
+
+
+    // Get the rank of the process
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    
+    // Get the number of processes
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    int number;
+    if(world_rank == 0){
+	number = -1;
+	MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+    }else if(world_rank == 1){
+	    MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	    // Print off a hello world message
+	    printf("Process 1 received number %d from process 0\n", number);
+    }
+
+    // Finalize the MPI environment.
+    MPI_Finalize();
+}
