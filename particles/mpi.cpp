c0d7efaade86e69086ac45e3478a7964d071b328
hermetico <yojmarinv@gmail.com>
2017-11-01 15:27:16 +0100

    testing mpi

--- particles/mpi.cpp
+++ particles/mpi.cpp
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include "common.h"
-
+#include <stdio.h>
 //
 //  benchmarking program
 //
@@ -39,6 +39,7 @@ int main( int argc, char **argv )
 	MPI_Init( &argc, &argv );
 	MPI_Comm_size( MPI_COMM_WORLD, &n_proc );
 	MPI_Comm_rank( MPI_COMM_WORLD, &rank );
+	std::cout << "proces " << rank << " of "<< n_proc << " processes" << std::endl;
 	
 	//
 	//  allocate generic resources
