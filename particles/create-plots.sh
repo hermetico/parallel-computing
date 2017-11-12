587397c99cccf6e53272e68b9c3f79c65f10361d
hermetico <yojmarinv@gmail.com>
2017-11-12 18:27:10 +0100

    data collected and plots

--- particles/create-plots.sh
+++ particles/create-plots.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 PLOT=serial.eps
 
 gnuplot << GPLOT
@@ -7,7 +8,37 @@ set xlabel "log(n)"
 set ylabel "log(seconds)"
 set output "$PLOT"
 
-plot "data-serial.txt" using (log(\$1)/log(2)):2 title "Serial implementation", \
-     "$BFSDATAFILE" using (log(\$1)/log(2)):2 title "BFS", \
-     "$DFSDATADIRFILE" using (log(\$1)/log(2)):2 title "DFS, alpha = 0.75"
+plot "data-serial.txt" using (log(\$1)/log(2)):(log(\$2)/log(2)) title "Serial implementation"
+GPLOT
+
+PLOT=openmp.eps
+gnuplot << GPLOT
+set terminal postscript eps enhanced color
+set xlabel "log(n)"
+set ylabel "log(seconds)"
+set output "$PLOT"
+
+plot "data-openmp.txt" using (log(\$1)/log(2)):(log(\$2)/log(2)) title "Openmp 1 thread",
+    "data-openmp-2.txt" using (log(\$1)/log(2)):(log(\$2)/log(2)) title "Openmp 2 threads",
+    "data-openmp-4.txt" using (log(\$1)/log(2)):(log(\$2)/log(2)) title "Openmp 4 threads",
+    "data-openmp-6.txt" using (log(\$1)/log(2)):(log(\$2)/log(2)) title "Openmp 6 threads",
+    "data-openmp-12.txt" using (log(\$1)/log(2)):(log(\$2)/log(2)) title "Openmp 12 threads",
+    "data-openmp-16.txt" using (log(\$1)/log(2)):(log(\$2)/log(2)) title "Openmp 16 threads",
+    "data-openmp-24.txt" using (log(\$1)/log(2)):(log(\$2)/log(2)) title "Openmp 24 threads"
+GPLOT
+
+PLOT=mpi.eps
+gnuplot << GPLOT
+set terminal postscript eps enhanced color
+set xlabel "log(n)"
+set ylabel "log(seconds)"
+set output "$PLOT"
+
+plot "data-mpi.txt" using (log(\$1)/log(2)):(log(\$2)/log(2)) title "MPI 1 thread",
+    "data-mpi-2.txt" using (log(\$1)/log(2)):(log(\$2)/log(2)) title "MPI 2 threads",
+    "data-mpi-4.txt" using (log(\$1)/log(2)):(log(\$2)/log(2)) title "MPI 4 threads",
+    "data-mpi-6.txt" using (log(\$1)/log(2)):(log(\$2)/log(2)) title "MPI 6 threads",
+    "data-mpi-12.txt" using (log(\$1)/log(2)):(log(\$2)/log(2)) title "MPI 12 threads",
+    "data-mpi-18.txt" using (log(\$1)/log(2)):(log(\$2)/log(2)) title "MPI 18 threads",
+    "data-mpi-24.txt" using (log(\$1)/log(2)):(log(\$2)/log(2)) title "MPI 24 threads"
 GPLOT
---
+++ particles/data/create-plots.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+PLOT=serial.eps
+
+gnuplot << GPLOT
+set terminal postscript eps enhanced color
+set xlabel "log(n)"
+set ylabel "log(seconds)"
+set output "$PLOT"
+set key left top
+
+plot "data-serial.txt" using (log(\$1)/log(2)):(log(\$2)/log(2)) title "Serial implementation"
+GPLOT
+
+PLOT=openmp.eps
+
+gnuplot << GPLOT
+set terminal postscript eps enhanced color
+set xlabel "log(n)"
+set ylabel "log(seconds)"
+set output "$PLOT"
+set key left top
+
+plot "data-openmp.txt" using (log(\$1)/log(2)):(log(\$2)/log(2)) title "Openmp 1 thread", \
+    "data-openmp-2.txt" using (log(\$1)/log(2)):(log(\$2)/log(2)) title "Openmp 2 threads", \
+    "data-openmp-4.txt" using (log(\$1)/log(2)):(log(\$2)/log(2)) title "Openmp 4 threads", \
+    "data-openmp-6.txt" using (log(\$1)/log(2)):(log(\$2)/log(2)) title "Openmp 6 threads", \
+    "data-openmp-12.txt" using (log(\$1)/log(2)):(log(\$2)/log(2)) title "Openmp 12 threads", \
+    "data-openmp-16.txt" using (log(\$1)/log(2)):(log(\$2)/log(2)) title "Openmp 16 threads", \
+    "data-openmp-24.txt" using (log(\$1)/log(2)):(log(\$2)/log(2)) title "Openmp 24 threads"
+GPLOT
+
+PLOT=mpi.eps
+
+gnuplot << GPLOT
+set terminal postscript eps enhanced color
+set xlabel "log(n)"
+set ylabel "log(seconds)"
+set output "$PLOT"
+set key left top
+
+plot "data-mpi.txt" using (log(\$1)/log(2)):(log(\$2)/log(2)) title "MPI 1 processes", \
+    "data-mpi-2.txt" using (log(\$1)/log(2)):(log(\$2)/log(2)) title "MPI 2 processes", \
+    "data-mpi-4.txt" using (log(\$1)/log(2)):(log(\$2)/log(2)) title "MPI 4 processes", \
+    "data-mpi-6.txt" using (log(\$1)/log(2)):(log(\$2)/log(2)) title "MPI 6 processes", \
+    "data-mpi-12.txt" using (log(\$1)/log(2)):(log(\$2)/log(2)) title "MPI 12 processes", \
+    "data-mpi-18.txt" using (log(\$1)/log(2)):(log(\$2)/log(2)) title "MPI 18 processes", \
+    "data-mpi-24.txt" using (log(\$1)/log(2)):(log(\$2)/log(2)) title "MPI 24 processes"
+GPLOT
